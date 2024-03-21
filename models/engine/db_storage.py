0c448dd9ee75b7ddcb2a56fedd7a7f53fed22038
Alaa Fayed <alaafayed810@gmail.com>
2024-03-21 17:55:08 +0200

    all

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,7 +2,7 @@
 from os import getenv
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from models.user import User
 from models.place import Place
 from models.state import State

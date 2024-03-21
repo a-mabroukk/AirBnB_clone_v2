daa984bcff2ed01126aec40895cb3f09e500ccd6
Alaa Fayed <alaafayed810@gmail.com>
2024-03-21 04:59:28 +0200

    6

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
 
 
 class Amenity(BaseModel, Base):
     """Amenity class that inherits from BaseModel and Base"""
     name = ""
-

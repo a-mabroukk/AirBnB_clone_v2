5e55e495e03d20be9521c27cfb66354da98f4cb2
Alaa Fayed <alaafayed810@gmail.com>
2024-03-21 04:13:40 +0200

    7

--- models/__init__.py
+++ models/__init__.py
@@ -7,7 +7,7 @@ env_var = environ['HBNB_TYPE_STORAGE']
 if env_var == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
-    storage.reload()
+
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()

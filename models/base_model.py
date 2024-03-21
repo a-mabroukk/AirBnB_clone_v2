64b19cb8393b5911b028fe084e88e049ab6eb8a3
Alaa Fayed <alaafayed810@gmail.com>
2024-03-21 04:44:36 +0200

    6

--- models/base_model.py
+++ models/base_model.py
@@ -7,6 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), nullable=False, primary_key=True)
@@ -27,7 +28,7 @@ class BaseModel:
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
             self.__dict__.update(kwargs)
-        
+
         for key, value in kwargs.items():
             if not hasattr(self, key):
                 setattr(self, key, value)
@@ -61,6 +62,7 @@ class BaseModel:
 
         def delete(self):
             """public instance method to delete
-            the current instance from the storag by calling the method delete"""
+            the current instance from the storag by calling the method delete
+            """
             from models import storage
             models.storage.delete(self)
--- models/state.py
+++ models/state.py
@@ -11,8 +11,8 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
 
-    cities = relationship("City", backref="state", cascade="all, delete-orphan")
-
+    cities = relationship(
+        "City", backref="state", cascade="all, delete-orphan")
 
     def cities(self):
         """getter attribute cities that returns the list of City instances"""

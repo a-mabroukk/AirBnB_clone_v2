e7f6e99039a2e82be76a0b02e8d17494cf5c98e0
MohamedAbd-elrady <mmohammedabdelrady@gmail.com>
2024-03-23 12:49:52 +0200

    models

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+'''
+    Implementation of the Amenity class
+'''
+from os import getenv
 from models.base_model import BaseModel, Base
+from models.place import place_amenity
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
-    """Amenity class that inherits from BaseModel and Base"""
+    '''
+        Implementation for the Amenities.
+    '''
     __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-
-    place_amenities = relationship("Place", secondary=place_amenity)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary=place_amenity,
+                                       back_populates="amenities")
+    else:
+        name = ""
--- models/state.py
+++ models/state.py
@@ -1,25 +1,37 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer
+'''
+    Implementation of the State class
+'''
+from os import getenv
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.city import City
+from models.base_model import BaseModel, Base
+import models
 
 
 class State(BaseModel, Base):
-    """ State class """
+    '''
+        Implementation for the State.
+        Create relationship between class State (parent) to City (child)
+    '''
     __tablename__ = "states"
-    name = Column(String(128), nullable=False)
 
-    cities = relationship(
-        "City", backref="state", cascade="all, delete")
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state",
+                              cascade="all, delete, delete-orphan")
+    else:
+        name = ""
 
-    @property
-    def cities(self):
-        """getter attribute cities that returns the list of City instances"""
-        from models import storage
-        all_cities = models.storage.all(City)
-        for obj, instance in all_cities.items():
-            if isinstance(instance, City) and City.state_id == State.id:
-                all_cities.append(obj)
-        return all_cities
+        @property
+        def cities(self):
+            '''
+                Return list of city instances if City.state_id==current
+                State.id
+                FileStorage relationship between State and City
+            '''
+            list_cities = []
+            for city in models.storage.all("City").values():
+                if city.state_id == self.id:
+                    list_cities.append(city)
+            return list_cities

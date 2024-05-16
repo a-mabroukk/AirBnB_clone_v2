6bd220580b5acd7ac75eed48a3c29e3466bc2dd2
Alaa Fayed <alaafayed810@gmail.com>
2024-05-16 03:37:32 +0200

    2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,20 +9,20 @@ env.user = "ubuntu"
 
 def do_deploy(archive_path):
     """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    if not exists(archive_path):
         return False
-    try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
+    if archive_path:
+        filename = archive_path.split("/")[-1]
+        extract = filename.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('mkdir -p {}{}/'.format(path, extract))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(filename, path, extract))
+        run('rm /tmp/{}'.format(filename))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, extract))
+        run('rm -rf {}{}/web_static'.format(path, extract))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        run('ln -s {}{}/ /data/web_static/current'.format(path, extract))
         return True
-    except:
+    else:
         return False
